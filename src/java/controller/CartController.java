ea02d1936c086d4969d4112ec21ad83ee2d9e23e
NamhocFPT <hoainguyennam10@gmail.com>
2025-02-13 23:03:13 +0700

    Update fix Rename and local ava

--- src/java/controller/CartController.java
+++ src/java/controller/CartController.java
@@ -2,7 +2,6 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
  */
-
 package controller;
 
 import entity.Cart;
@@ -21,21 +20,22 @@ import model.DAOProducts;
  *
  * @author HP
  */
-@WebServlet(name="CartController", urlPatterns={"/CartController"})
+@WebServlet(name = "CartController", urlPatterns = {"/CartController"})
 public class CartController extends HttpServlet {
+
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
             String service = request.getParameter("service");
             DAOProducts daoPro = new DAOProducts();
             HttpSession session = request.getSession();
-            
+            int cartItemPos = Integer.parseInt(request.getParameter("CartItemPos"));
             Cart cart = (Cart) session.getAttribute("cart");
             if (cart == null) {
                 cart = new Cart();
             }
-            
+
             if (service.equals("add2cart")) {
                 String username = (String) session.getAttribute("username");
                 if (username == null) {
@@ -43,7 +43,7 @@ public class CartController extends HttpServlet {
                 }
                 int ProductID = Integer.parseInt(request.getParameter("pid"));
 
-                    CartItem ci = new CartItem(daoPro.getProductById(ProductID), 1);
+                CartItem ci = new CartItem(daoPro.getProductById(ProductID), 1);
                 cart.addItemToCart(ci);
                 response.sendRedirect("ShopController?service=pagination&pageid=1");
             }
@@ -51,11 +51,11 @@ public class CartController extends HttpServlet {
             if (service.equals("showShoppingCart")) {
                 response.sendRedirect("Shopping-cart.jsp");
             }
-            
+
             if (service.equals("deleteCartItem")) {
-                int CartItemPos = Integer.parseInt(request.getParameter("CartItemPos"));
-                int ProductID = Integer.parseInt(request.getParameter("pid"));
-                    CartItem ci = new CartItem(daoPro.getProductById(ProductID), 1);
+
+                int productID = Integer.parseInt(request.getParameter("pid"));
+                CartItem ci = new CartItem(daoPro.getProductById(productID), 1);
                 cart.addItemToCart(ci);
                 response.sendRedirect("ShopController?service=pagination&pageid=1");
             }
@@ -63,13 +63,12 @@ public class CartController extends HttpServlet {
             if (service.equals("showShoppingCart")) {
                 response.sendRedirect("Shopping-cart.jsp");
             }
-            
+
             if (service.equals("deleteCartItem")) {
-                int CartItemPos = Integer.parseInt(request.getParameter("CartItemPos"));
-                cart.updateCart(CartItemPos, cart.getItemsList().get(CartItemPos).getQuantity()-2);
+                cart.updateCart(cartItemPos, cart.getItemsList().get(cartItemPos).getQuantity() - 2);
                 response.sendRedirect("Shopping-cart.jsp");
             }
-            
+
             if (service.equals("UpdateCart")) {
                 for (int i = 0; i < cart.getItemsList().size(); i++) {
                     int newQuantity = Integer.parseInt(request.getParameter("updateQuantity_" + i));
@@ -81,8 +80,9 @@ public class CartController extends HttpServlet {
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /** 
+    /**
      * Handles the HTTP <code>GET</code> method.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -90,12 +90,13 @@ public class CartController extends HttpServlet {
      */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         processRequest(request, response);
-    } 
+    }
 
-    /** 
+    /**
      * Handles the HTTP <code>POST</code> method.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -103,12 +104,13 @@ public class CartController extends HttpServlet {
      */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         processRequest(request, response);
     }
 
-    /** 
+    /**
      * Returns a short description of the servlet.
+     *
      * @return a String containing servlet description
      */
     @Override
